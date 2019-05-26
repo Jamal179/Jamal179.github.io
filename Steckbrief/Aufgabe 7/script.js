0ab1c543a845c91ff8a8e1a6c03fc60d7bfce13b
Jamal <jamal.ulbricht@aol.de>
2019-05-26 12:41:22 +0200

    Aufgabe 7

--- Steckbrief/Aufgabe 7/script.js
+++ Steckbrief/Aufgabe 7/script.js
@@ -1,18 +1,42 @@
 window.addEventListener("load", geladen);
 function geladen() {
-    let hufeisen = "Seidentofu wurde geladen";
+    window.prompt("Hast du Lust zwei unnötige Spiele zu spielen?");
+    let hochHa = "Baue ein Hochhaus!";
+    console.log("Seidentofu wurde geladen");
     let elementErstellenZwei = document.createElement("div");
     document.getElementById("Fischkrrr").appendChild(elementErstellenZwei);
-    let variableY = `<p> ${hufeisen}</p>`;
+    let variableY = `<h1> ${hochHa}</h1>`;
     elementErstellenZwei.innerHTML = variableY;
+    document.getElementById('classever').addEventListener('click', changeHTMLClass);
+}
+function changeHTMLClass() {
+    document.getElementById("classever").className = "neueKlasse";
 }
 console.log("Valar Morghulis");
 window.onload = function () {
     console.log("Valar Dohaeris");
-    //function changeIt() {
-    // ('Warum verstehst du das wort >NICHT< nicht?!');
-    //}
-    //changeIt()
+    function Aaa() {
+        let hufeisen = "Die Anwort auf alles ist:";
+        let elementErstellenZwei = document.createElement("div");
+        document.getElementById("zahleeen").appendChild(elementErstellenZwei);
+        let variableY = `<p> ${hufeisen}</p>`;
+        elementErstellenZwei.innerHTML = variableY;
+    }
+    function Muh(numeroUno = 12, numeroDos = 20) {
+        let summee = numeroUno + numeroDos;
+        console.log(summee);
+        return summee;
+    }
+    Muh();
+    function Neuu() {
+        document.getElementById("booBut").addEventListener("click", Neuu);
+        let nochNeZahl = 15;
+        let nochEineZahl = 100;
+        let variBo = (nochEineZahl > nochNeZahl);
+        console.log(variBo);
+    }
+    Neuu();
+    Aaa();
     function berechnen() {
         let zahl = 26;
         let zahlen = 17;
@@ -26,31 +50,49 @@ window.onload = function () {
     }
     berechnen();
     function schrift() {
-        let text = "Steingabelfuß ";
-        let texte = "Siamesischer Kampffisch";
+        let text = "Versuche den Button zu  ";
+        let texte = "zu fangen!";
         var beide = text + texte;
         console.log(beide);
         let elementErstellenZwei = document.createElement("div");
-        document.getElementById("zahleeen").appendChild(elementErstellenZwei);
-        let variableY = `<p> ${beide}</p>`;
+        document.getElementById("divTwo").appendChild(elementErstellenZwei);
+        let variableY = `<h2> ${beide}</h2>`;
         elementErstellenZwei.innerHTML = variableY;
     }
     schrift();
-    document.getElementById("Banane").addEventListener("click", aenderung);
+    document.getElementById("ButtonUno").addEventListener("click", aenderung);
     function aenderung() {
         document.getElementById("Hus").innerHTML = "";
-        let texter = "Baumus";
-        console.log(texter);
+        let texter = "GAME OVER";
+        console.log("Du warst du langsam! Du hast Verloren");
         let elementErstellenZwei = document.createElement("div");
         document.getElementById("Hus").appendChild(elementErstellenZwei);
         let variableY = `<p> ${texter}</p>`;
         elementErstellenZwei.innerHTML = variableY;
     }
+    function schriftZahl() {
+        let Zahlie = 3;
+        let Schriftie = " Chinesen mit nem' Kontrabass";
+        console.log(Zahlie + Schriftie);
+    }
+    schriftZahl();
+    document.getElementById("ButtonDos").addEventListener("click", butti);
+    function butti() {
+        document.getElementById("Fus").innerHTML = "";
+        let butt = document.createElement("button");
+        let stang = "☐ ☐ ☐ ☐ ☐ ☐";
+        console.log("Glückwunsch! Du hast ein neues Stockwerk gebaut");
+        let elementErstellene = document.createElement("div");
+        document.getElementById("ButtonDres").appendChild(elementErstellene);
+        let variabler = `<p> ${stang}</p>`;
+        elementErstellene.innerHTML = variabler;
+    }
+    butti();
     function neu() {
         document.getElementById("zusatz");
-        let zzz = "Noch ein Button";
+        let zzz = "Fang mich!";
         let elementErstellenVier = document.createElement("button");
-        document.getElementById("zahleeen").appendChild(elementErstellenVier);
+        document.getElementById("zusatz").appendChild(elementErstellenVier);
         let variableY = `<p> ${zzz}</p>`;
         elementErstellenVier.innerHTML = variableY;
         document.getElementById("zusatz").addEventListener("click", funk);
@@ -58,24 +100,14 @@ window.onload = function () {
     neu();
     function funk() {
         document.getElementById("zusatz").innerHTML = "";
-        let XXX = "Geändert";
+        let texter = "Daneben!";
+        console.log(texter);
         let elementErstellenFunf = document.createElement("div");
-        document.getElementById("zusatz").appendChild(elementErstellenFunf);
-        let variableQ = `<p> ${XXX}</p>`;
+        document.getElementById("Hus").appendChild(elementErstellenFunf);
+        let variableQ = `<p> ${texter}</p>`;
         elementErstellenFunf.innerHTML = variableQ;
+        document.getElementById("zusatz").addEventListener("click", butti);
     }
 };
-/*
-document.getElementById("Wald").addEventListener("click", aenderungs);
-function aenderungs(){
-   
-    document.getElementById("Kalt").innerHTML = "";
-    let texters : string = "Baumus";
-    console.log(texters);
-    let elementErstellenDrei : HTMLButtonElement = document.createElement("button");
-    document.getElementById("Kalt").appendChild(elementErstellenDrei);
-    let variableZ : string = `<button> ${texters}</button>`;
-    elementErstellenDrei.innerHTML = variableZ;
-    */
 /* Typescript is dark and full of terror */
 //# sourceMappingURL=script.js.map
--- Steckbrief/Aufgabe 7/script.ts
+++ Steckbrief/Aufgabe 7/script.ts
@@ -1,26 +1,61 @@
 window.addEventListener("load", geladen);
 function geladen (){
-    let hufeisen : string = "Seidentofu wurde geladen"
+    window.prompt("Hast du Lust zwei unnötige Spiele zu spielen?");
+    let hochHa : string = "Baue ein Hochhaus!";
+    console.log("Seidentofu wurde geladen");
     let elementErstellenZwei : HTMLDivElement = document.createElement("div");
-    document.getElementById("Fischkrrr").appendChild(elementErstellenZwei)
-    let variableY : string = `<p> ${hufeisen}</p>`
-    elementErstellenZwei.innerHTML = variableY
+    document.getElementById("Fischkrrr").appendChild(elementErstellenZwei);
+    let variableY : string = `<h1> ${hochHa}</h1>`;
+    elementErstellenZwei.innerHTML = variableY;
+    document.getElementById('classever').addEventListener('click', changeHTMLClass);
 }
+function changeHTMLClass(){
+    document.getElementById("classever").className = "neueKlasse";
+}
+
 
 
 
 console.log("Valar Morghulis");
 
+
 window.onload = function () {
     console.log("Valar Dohaeris");
   
 
-//function changeIt() {
-   // ('Warum verstehst du das wort >NICHT< nicht?!');
 
-//}
-//changeIt()
 
+function Aaa (){
+    let hufeisen : string = "Die Anwort auf alles ist:";
+    let elementErstellenZwei : HTMLDivElement = document.createElement("div");
+    document.getElementById("zahleeen").appendChild(elementErstellenZwei);
+    let variableY : string = `<p> ${hufeisen}</p>`;
+    elementErstellenZwei.innerHTML = variableY;
+}
+
+function Muh(numeroUno=12, numeroDos=20 ){
+    let summee:number = numeroUno + numeroDos; 
+    console.log(summee)
+    return summee;
+   
+}
+Muh()
+
+
+
+
+function Neuu(){
+    document.getElementById("booBut").addEventListener("click", Neuu);
+    let nochNeZahl : Number = 15;
+    let nochEineZahl : Number = 100;
+    let variBo : Boolean = (nochEineZahl > nochNeZahl);
+    console.log(variBo);
+  
+}
+Neuu()
+
+
+Aaa()
 
 function berechnen(){
     let zahl : number = 26;
@@ -29,72 +64,99 @@ function berechnen(){
     let summe : number = zahl + zahlen; 
     console.log(summe);
     let elementErstellen : HTMLDivElement = document.createElement("div");
-    document.getElementById("zahleeen").appendChild(elementErstellen)
-    let variableX : string = `<p> ${summe}</p>`
-    elementErstellen.innerHTML = variableX
+    document.getElementById("zahleeen").appendChild(elementErstellen);
+    let variableX : string = `<p> ${summe}</p>`;
+    elementErstellen.innerHTML = variableX;
 }
+
+
 berechnen()
 
 function schrift(){
-let text : string = "Steingabelfuß ";
-let texte : string = "Siamesischer Kampffisch";
+let text : string = "Versuche den Button zu  ";
+let texte : string = "zu fangen!";
 var beide : string = text + texte;
 console.log(beide);
 let elementErstellenZwei : HTMLDivElement = document.createElement("div");
-document.getElementById("zahleeen").appendChild(elementErstellenZwei)
-let variableY : string = `<p> ${beide}</p>`
-elementErstellenZwei.innerHTML = variableY
+document.getElementById("divTwo").appendChild(elementErstellenZwei);
+let variableY : string = `<h2> ${beide}</h2>`;
+elementErstellenZwei.innerHTML = variableY;
 
 }
+
+
 schrift()
 
-document.getElementById("Banane").addEventListener("click", aenderung);
+document.getElementById("ButtonUno").addEventListener("click", aenderung);
 function aenderung(){
    
     document.getElementById("Hus").innerHTML = "";
-    let texter : string = "Baumus";
-    console.log(texter);
+    let texter : string = "GAME OVER";
+    console.log("Du warst du langsam! Du hast Verloren");
     let elementErstellenZwei : HTMLDivElement = document.createElement("div");
-    document.getElementById("Hus").appendChild(elementErstellenZwei)
-    let variableY : string = `<p> ${texter}</p>`
-    elementErstellenZwei.innerHTML = variableY
+    document.getElementById("Hus").appendChild(elementErstellenZwei);
+    let variableY : string = `<p> ${texter}</p>`;
+    elementErstellenZwei.innerHTML = variableY;
 
 }
+function schriftZahl(){
+    let Zahlie : number = 3 ;
+    let Schriftie : string = " Chinesen mit nem' Kontrabass";
+    console.log(Zahlie + Schriftie);
+}
+schriftZahl()
+
+document.getElementById("ButtonDos").addEventListener("click", butti);
+function butti(){
+   
+    document.getElementById("Fus").innerHTML = "";
+    let butt : HTMLButtonElement = document.createElement("button");
+    let stang : string = "☐ ☐ ☐ ☐ ☐ ☐";
+    console.log("Glückwunsch! Du hast ein neues Stockwerk gebaut");
+    let elementErstellene : HTMLDivElement = document.createElement("div");
+    document.getElementById("ButtonDres").appendChild(elementErstellene);
+    let variabler : string = `<p> ${stang}</p>`;
+    elementErstellene.innerHTML = variabler;
+}
+butti ()
 
 function neu(){
-document.getElementById("zusatz")
-let zzz : string = "Noch ein Button"
+document.getElementById("zusatz");
+let zzz : string = "Fang mich!";
 let elementErstellenVier : HTMLButtonElement = document.createElement("button");
-document.getElementById("zahleeen").appendChild(elementErstellenVier)
-let variableY : string = `<p> ${zzz}</p>`
-elementErstellenVier.innerHTML = variableY
+document.getElementById("zusatz").appendChild(elementErstellenVier);
+let variableY : string = `<p> ${zzz}</p>`;
+elementErstellenVier.innerHTML = variableY;
 document.getElementById("zusatz").addEventListener("click", funk);
+
+
 }
 neu()
 
+
 function funk(){
-    document.getElementById("zusatz").innerHTML = "";    
-    let XXX : string = "Geändert";
+   
+    document.getElementById("zusatz").innerHTML = "";
+    let texter : string = "Daneben!";
+    console.log(texter);
     let elementErstellenFunf : HTMLDivElement = document.createElement("div");
-    document.getElementById("zusatz").appendChild(elementErstellenFunf);
-let variableQ : string = `<p> ${XXX}</p>`;
-elementErstellenFunf.innerHTML = variableQ
-}
+    document.getElementById("Hus").appendChild(elementErstellenFunf);
+    let variableQ : string = `<p> ${texter}</p>`;
+    elementErstellenFunf.innerHTML = variableQ;
+    document.getElementById("zusatz").addEventListener("click", butti);
+
 
 }
-/*
-document.getElementById("Wald").addEventListener("click", aenderungs);
-function aenderungs(){
-   
-    document.getElementById("Kalt").innerHTML = "";
-    let texters : string = "Baumus";
-    console.log(texters);
-    let elementErstellenDrei : HTMLButtonElement = document.createElement("button");
-    document.getElementById("Kalt").appendChild(elementErstellenDrei);
-    let variableZ : string = `<button> ${texters}</button>`;
-    elementErstellenDrei.innerHTML = variableZ;
-    */
+
+
+
+}
+
+
 
 
 
    /* Typescript is dark and full of terror */
+
+
+
